943796e771df63afe2bc6e68743bd3a7abf191b4
Rod Conan <rconan@gmto.org>
2021-01-12 22:55:14 -0800

    INITIAL REVISION

---
+++ src/io.rs
@@ -0,0 +1,51 @@
+use serde::Deserialize;
+
+/// Fem input/output data properties
+#[derive(Deserialize, Debug, Clone)]
+pub struct Properties {
+    #[serde(rename = "nodeID")]
+    pub node_id: Option<Vec<u32>>,
+    #[serde(rename = "csLabel")]
+    pub cs_label: Option<String>,
+    pub location: Option<Vec<f64>>,
+    pub component: Option<Vec<i32>>,
+    pub components: Option<Vec<f64>>,
+    pub area: Option<Vec<f64>>,
+}
+/// Fem input/output data
+#[derive(Deserialize, Debug, Clone)]
+pub struct IOData {
+    types: String,
+    #[serde(rename = "exciteIDs")]
+    excite_ids: Option<Vec<u32>>,
+    pub descriptions: String,
+    indices: Vec<u32>,
+    pub properties: Properties,
+}
+/// Fem input/output 2 states: on or off
+#[derive(Deserialize, Debug, Clone)]
+#[serde(untagged)]
+pub enum IO {
+    On(IOData),
+    Off(IOData),
+}
+impl IO {
+    pub fn switch_off(self) -> Self {
+        match self {
+            IO::On(data) => IO::Off(data),
+            IO::Off(_) => self
+        }
+    }
+    pub fn switch_on(self) -> Self {
+        match self {
+            IO::Off(data) => IO::On(data),
+            IO::On(_) => self
+        }
+    }
+    pub fn is_on(&self) -> bool {
+        match self {
+            IO::On(_) => true,
+            IO::Off(_) => false
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,86 @@
+use serde_pickle as pkl;
+use std::collections::BTreeMap;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+pub mod io;
+pub use io::{IOData, IO};
+
+pub fn load_io<P: AsRef<Path>>(path: P) -> Result<BTreeMap<String, Vec<IO>>, Box<dyn Error>> {
+    let f = File::open(path)?;
+    let r = BufReader::with_capacity(1_000_000, f);
+    Ok(pkl::from_reader(r)?)
+}
+
+pub trait IOTraits {
+    fn n(&self) -> usize;
+    fn n_on(&self) -> usize;
+    fn off(&mut self) -> &mut Self;
+    fn on(&mut self, io_name: &str) -> &mut Self;
+    fn on_by<F>(&mut self, io_name: &str, pred: F) -> &mut Self
+    where
+        F: Fn(&IOData) -> bool;
+    fn io(&self, io_name: &str) -> Vec<&IOData>;
+}
+
+impl IOTraits for BTreeMap<String, Vec<IO>> {
+    /// Gets the number of `io`
+    fn n(&self) -> usize {
+        self.values().fold(0, |a, x| a + x.len())
+    }
+    /// Gets the number of `io` that are turned on
+    fn n_on(&self) -> usize {
+        self.values().fold(0, |a, x| {
+            a + x.iter().fold(0, |a, x| a + x.is_on() as usize)
+        })
+    }
+    /// Turns off all `io`
+    fn off(&mut self) -> &mut Self {
+        self.values_mut().for_each(|value| {
+            value.iter_mut().for_each(|io| {
+                *io = io.clone().switch_off();
+            })
+        });
+        self
+    }
+    /// Turns on the given `io`
+    fn on(&mut self, io_name: &str) -> &mut Self {
+        self.get_mut(io_name)
+            .expect(&format!("IO {} not found", io_name))
+            .iter_mut()
+            .for_each(|io| {
+                *io = io.clone().switch_on();
+            });
+        self
+    }
+    /// Turns on the given output for entries that match the give predicate
+    fn on_by<F>(&mut self, io_name: &str, pred: F) -> &mut Self
+    where
+        F: Fn(&IOData) -> bool,
+    {
+        self.get_mut(io_name)
+            .expect(&format!("IO {} not found", io_name))
+            .iter_mut()
+            .for_each(|io| {
+                if let IO::Off(v) = io {
+                    if pred(v) {
+                        *io = IO::On(v.clone());
+                    }
+                }
+            });
+        self
+    }
+    /// Returns the turned-on entries of the requested `io`
+    fn io(&self, io_name: &str) -> Vec<&IOData> {
+        self.get(io_name)
+            .expect(&format!("Input {} not found", io_name))
+            .iter()
+            .filter_map(|io| match io {
+                IO::On(v) => Some(v),
+                IO::Off(_) => None,
+            })
+            .collect()
+    }
+}
