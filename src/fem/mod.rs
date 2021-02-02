29e6adde0b25d695933c36bc01cc54c423e66b61
Rod Conan <rconan@gmto.org>
2021-02-02 15:03:58 -0800

    FEM MODULE
    Move fem.rs to a module directory
    Replace inputs and outputs maps with enums

--- src/fem/mod.rs
+++ src/fem/mod.rs
@@ -1,15 +1,16 @@
 use super::Bilinear;
-use super::IOTraits;
 use super::IO;
 use anyhow::{Context, Result};
 use nalgebra as na;
+use serde;
 use serde::Deserialize;
 use serde_pickle as pkl;
-use std::collections::BTreeMap;
 use std::fmt;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
+
+pub mod fem_io;
 
 /// Finite Element Model
 #[derive(Deserialize, Debug)]
@@ -18,9 +19,9 @@ pub struct FEM {
     #[serde(rename = "modelDescription")]
     pub model_description: String,
     /// inputs properties
-    pub inputs: BTreeMap<String, Vec<IO>>,
+    pub inputs: Vec<Option<fem_io::Inputs>>,
     /// outputs properties
-    pub outputs: BTreeMap<String, Vec<IO>>,
+    pub outputs: Vec<Option<fem_io::Outputs>>,
     /// mode shapes eigen frequencies [Hz]
     #[serde(rename = "eigenfrequencies")]
     pub eigen_frequencies: Vec<f64>,
@@ -42,7 +43,8 @@ impl FEM {
     {
         let f = File::open(path).context(format!("File {} not found", path))?;
         let r = BufReader::with_capacity(1_000_000, f);
-        Ok(pkl::from_reader(r).context(format!("Failed to load {}", path))?)
+        let v: serde_pickle::Value = serde_pickle::from_reader(r).unwrap();
+        Ok(pkl::from_value(v).context(format!("Failed to load {}", path))?)
     }
     /// Gets the number of modes
     pub fn n_modes(&self) -> usize {
@@ -55,20 +57,47 @@ impl FEM {
             .map(|x| 2.0 * std::f64::consts::PI * x)
             .collect()
     }
+    pub fn n_inputs(&self) -> usize {
+        self.inputs
+            .iter()
+            .filter_map(|x| x.as_ref())
+            .fold(0usize, |a, x| a + x.len())
+    }
+    pub fn n_outputs(&self) -> usize {
+        self.outputs
+            .iter()
+            .filter_map(|x| x.as_ref())
+            .fold(0usize, |a, x| a + x.len())
+    }
+    pub fn keep_input(&mut self, id: usize) {
+        self.inputs.iter_mut().enumerate().for_each(|(k,x)| {
+            if k!=id {
+                *x = None;
+            }
+        });
+    }
+    pub fn keep_output(&mut self, id: usize) {
+        self.outputs.iter_mut().enumerate().for_each(|(k,x)| {
+            if k!=id {
+                *x = None;
+            }
+        });
+    }
     /// Returns the inputs 2 modes transformation matrix for the turned-on inputs
     pub fn inputs2modes(&mut self) -> Vec<f64> {
         let indices: Vec<u32> = self
             .inputs
-            .values()
+            .iter()
+            .filter_map(|x| x.as_ref())
             .flat_map(|v| {
-                v.iter().filter_map(|x| match x {
+                v.io().iter().filter_map(|x| match x {
                     IO::On(io) => Some(io.indices.clone()),
                     IO::Off(_) => None,
                 })
             })
             .flatten()
             .collect();
-        let n = self.inputs.n();
+        let n = self.inputs_to_modal_forces.len() / self.n_modes();
         self.inputs_to_modal_forces
             .chunks(n)
             .flat_map(|x| {
@@ -84,9 +113,10 @@ impl FEM {
         let n = self.n_modes();
         let q: Vec<_> = self.modal_disp_to_outputs.chunks(n).collect();
         self.outputs
-            .values()
+            .iter()
+            .filter_map(|x| x.as_ref())
             .flat_map(|v| {
-                v.iter().filter_map(|x| match x {
+                v.io().iter().filter_map(|x| match x {
                     IO::On(io) => Some(io.indices.clone()),
                     IO::Off(_) => None,
                 })
@@ -99,9 +129,9 @@ impl FEM {
     /// Returns the FEM static gain for the turned-on inputs and outputs
     pub fn static_gain(&mut self) -> na::DMatrix<f64> {
         let forces_2_modes =
-            na::DMatrix::from_row_slice(self.n_modes(), self.inputs.n_on(), &self.inputs2modes());
+            na::DMatrix::from_row_slice(self.n_modes(), self.n_inputs(), &self.inputs2modes());
         let modes_2_nodes =
-            na::DMatrix::from_row_slice(self.outputs.n_on(), self.n_modes(), &self.modes2outputs());
+            na::DMatrix::from_row_slice(self.n_outputs(), self.n_modes(), &self.modes2outputs());
         let d = na::DMatrix::from_diagonal(
             &na::DVector::from_row_slice(&self.eigen_frequencies_to_radians())
                 .map(|x| 1f64 / (x * x)),
@@ -112,10 +142,10 @@ impl FEM {
     pub fn state_space(&mut self, sampling_rate: f64) -> Vec<Bilinear> {
         let tau = 1. / sampling_rate;
         let modes_2_nodes =
-            na::DMatrix::from_row_slice(self.outputs.n_on(), self.n_modes(), &self.modes2outputs());
+            na::DMatrix::from_row_slice(self.n_outputs(), self.n_modes(), &self.modes2outputs());
         println!("modes 2 nodes: {:?}", modes_2_nodes.shape());
         let forces_2_modes =
-            na::DMatrix::from_row_slice(self.n_modes(), self.inputs.n_on(), &self.inputs2modes());
+            na::DMatrix::from_row_slice(self.n_modes(), self.n_inputs(), &self.inputs2modes());
         println!("forces 2 modes: {:?}", forces_2_modes.shape());
         let w = self.eigen_frequencies_to_radians();
         let zeta = &self.proportional_damping_vec;
@@ -151,6 +181,37 @@ impl FEM {
 }
 impl fmt::Display for FEM {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let ins = self
+            .inputs
+            .iter()
+            .filter_map(|x| x.as_ref())
+            .enumerate()
+            .map(|(k,x)| format!(" #{:02} {}", k, x))
+            .collect::<Vec<String>>()
+            .join("\n");
+        let outs = self
+            .outputs
+            .iter()
+            .filter_map(|x| x.as_ref())
+            .enumerate()
+            .map(|(k,x)| format!(" #{:02} {}", k, x))
+            .collect::<Vec<String>>()
+            .join("\n");
+        write!(
+            f,
+            " INPUTS:\n{}\n{:>29}: [{:5}]\n OUTPUTS:\n{}\n{:>29}: [{:5}]",
+            ins,
+            "Total",
+            self.n_inputs(),
+            outs,
+            "Total",
+            self.n_outputs()
+        )
+    }
+}
+/*
+impl fmt::Display for FEM {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let a = format!(" - number of inputs: {}", self.inputs.n());
         let b = format!(" - number of outputs: {}", self.outputs.n());
         let c = format!(" - number of modes: {}", self.n_modes());
@@ -182,3 +243,4 @@ impl fmt::Display for FEM {
         )
     }
 }
+*/
