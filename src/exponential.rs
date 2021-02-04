2ac65faccfd49b2fde2ae8bfc87d93ee87af7054
Rod Conan <rconan@gmto.org>
2021-02-03 21:11:05 -0800

    DISCRETIZATION
    Added analytic matrix exponential continuous to discrete transformation

--- src/exponential.rs
+++ src/exponential.rs
@@ -1,8 +1,9 @@
 use nalgebra::Matrix2;
 use serde::Serialize;
+use num_complex::Complex;
 
 #[derive(Debug,Serialize)]
-pub struct Bilinear {
+pub struct Exponential {
     pub tau: f64,
     pub q: (f64, f64, f64, f64),
     pub m: (f64, f64, f64, f64),
@@ -11,7 +12,7 @@ pub struct Bilinear {
     pub y: Vec<f64>,
     x: (f64, f64),
 }
-impl Bilinear {
+impl Exponential {
     pub fn from_second_order(
         tau: f64,
         omega: f64,
@@ -19,17 +20,35 @@ impl Bilinear {
         continuous_bb: Vec<f64>,
         continuous_cc: Vec<f64>,
     ) -> Self {
+        /*
         let aa = Matrix2::<f64>::new(0., 1., -omega * omega, -2. * omega * zeta);
         let i = Matrix2::<f64>::identity();
         let qp = i + aa * (0.5 * tau);
         let iqm = (i - aa * (0.5 * tau)).try_inverse().unwrap();
         let q = (qp * iqm).as_slice().to_owned();
         let m = (iqm * tau.sqrt()).as_slice().to_owned();
+        */
+        let i = Matrix2::<f64>::identity();
+        let x = Complex { re: omega, im: 0. };
+        let y = Complex { re: zeta, im: 0. };
+        let ia = Matrix2::new((-2. * y / x).re, -1. / (x * x).re, 1., 0.);
+        let z = (x * x * (y * y - 1.)).sqrt();
+        let zmxy = z - x * y;
+        let zpxy = z + x * y;
+        let ezmxy = (tau * zmxy).exp();
+        let ezpxy = (-tau * zpxy).exp();
+        let ad = Matrix2::new(
+            ((zpxy * ezmxy + zmxy * ezpxy) / (2. * z)).re,
+            ((ezmxy - ezpxy) / (2. * z)).re,
+            (x * x * (ezpxy - ezmxy) / (2. * z)).re,
+            ((zmxy * ezmxy + zpxy * ezpxy) / (2. * z)).re,
+        );
+        let bd = ia * (ad - i)/tau.sqrt();
         let n = continuous_cc.len();
         Self {
             tau,
-            q: (q[0], q[2], q[1], q[3]),
-            m: (m[0], m[2], m[1], m[3]),
+            q: (ad[0], ad[2], ad[1], ad[3]),
+            m: (bd[0], bd[2], bd[1], bd[3]),
             b: continuous_bb,
             c: continuous_cc,
             y: vec![0.; n],
