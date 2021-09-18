0f9c9dce0f20d70f8e2c42c82fb86405fd6fc43d
Rod Conan <rconan@gmto.org>
2021-09-17 20:44:47 -0700

    ADD INPUTS/OUTPUTS TRANSFORMATION CAPABILITY
    Add `inputs_with` and `outputs_with` to pass matrices that are then
    applied to the corresponding inputs or outputs to transform them into another
    kind of data

--- src/dos/mod.rs
+++ src/dos/mod.rs
@@ -93,7 +93,7 @@ impl std::error::Error for StateSpaceError {
 }
 
 type Result<T> = std::result::Result<T, StateSpaceError>;
-type StateSpaceIO = Option<Vec<Tags>>;
+type StateSpaceIO = Vec<IO<Vec<f64>>>;
 
 fem_proc::match_maker! {}
 
@@ -102,8 +102,8 @@ fem_proc::match_maker! {}
 pub struct DiscreteStateSpace {
     sampling: Option<f64>,
     fem: Option<Box<fem::FEM>>,
-    u: StateSpaceIO,
-    y: StateSpaceIO,
+    u: Option<StateSpaceIO>,
+    y: Option<StateSpaceIO>,
     zeta: Option<f64>,
     eigen_frequencies: Option<Vec<(usize, f64)>>,
     max_eigen_frequency: Option<f64>,
@@ -167,34 +167,50 @@ impl DiscreteStateSpace {
         self
     }
     /// Sets the model inputs from a vector of [IO]
-    pub fn inputs(self, mut v_u: Vec<Tags>) -> Self {
-        let mut u = self.u;
-        if u.is_none() {
-            u = Some(v_u);
-        } else {
-            u.as_mut().unwrap().append(&mut v_u);
-        }
-        Self { u, ..self }
+    pub fn inputs(self, v_u: Vec<Tags>) -> Self {
+        let mut u = self.u.unwrap_or(vec![]);
+        v_u.iter().for_each(|t| {
+            let io: IO<Vec<f64>> = (t, Option::<Vec<f64>>::None).into();
+            u.push(io);
+        });
+
+        Self { u: Some(u), ..self }
+    }
+    pub fn inputs_with<'a>(self, v_u: Vec<Tags>, u_mat: impl Iterator<Item = &'a [f64]>) -> Self {
+        let mut u = self.u.unwrap_or(vec![]);
+        v_u.iter().zip(u_mat).for_each(|(t, mat)| {
+            let io: IO<Vec<f64>> = (t, Some(mat.to_owned())).into();
+            u.push(io);
+        });
+
+        Self { u: Some(u), ..self }
     }
     /// Sets the model inputs based on the outputs of another component
     pub fn inputs_from(self, element: &dyn IOTags) -> Self {
         self.inputs(element.outputs_tags())
     }
     /// Sets the model outputs from a vector of [IO]
-    pub fn outputs(self, mut v_y: Vec<Tags>) -> Self {
-        let mut y = self.y;
-        if y.is_none() {
-            y = Some(v_y);
-        } else {
-            y.as_mut().unwrap().append(&mut v_y);
-        }
-        Self { y, ..self }
+    pub fn outputs(self, v_y: Vec<Tags>) -> Self {
+        let mut y = self.y.unwrap_or(vec![]);
+        v_y.iter().for_each(|t| {
+            let io: IO<Vec<f64>> = (t, Option::<Vec<f64>>::None).into();
+            y.push(io);
+        });
+        Self { y: Some(y), ..self }
+    }
+    pub fn outputs_with<'a>(self, v_y: Vec<Tags>, y_mat: impl Iterator<Item = &'a [f64]>) -> Self {
+        let mut y = self.y.unwrap_or(vec![]);
+        v_y.iter().zip(y_mat).for_each(|(t, mat)| {
+            let io: IO<Vec<f64>> = (t, Some(mat.to_owned())).into();
+            y.push(io);
+        });
+        Self { y: Some(y), ..self }
     }
     /// Sets the model outputs based on the inputs of another component
     pub fn outputs_to(self, element: &dyn IOTags) -> Self {
         self.outputs(element.inputs_tags())
     }
-    fn select_fem_io(fem: &mut fem::FEM, dos_inputs: &[Tags], dos_outputs: &[Tags]) {
+    fn select_fem_io(fem: &mut fem::FEM, dos_inputs: &StateSpaceIO, dos_outputs: &StateSpaceIO) {
         log::debug!("\n## WHOLE FEM ##\n{}", fem);
         let inputs_idx: Vec<_> = fem
             .inputs
@@ -219,39 +235,73 @@ impl DiscreteStateSpace {
         fem.keep_inputs(&inputs_idx).keep_outputs(&outputs_idx);
         log::info!("\n## REDUCED FEM ##\n{}", fem);
     }
-    fn io2modes(fem: &fem::FEM, dos_inputs: &[Tags]) -> Result<Vec<f64>> {
+    fn io2modes(fem: &fem::FEM, dos_inputs: &StateSpaceIO) -> Result<Vec<f64>> {
         use crate::io::IO;
-        let indices: Vec<u32> = dos_inputs
+        let n_mode = fem.n_modes();
+        let n = fem.inputs_to_modal_forces.len() / fem.n_modes();
+        Ok(dos_inputs
             .iter()
             .map(|x| {
                 fem.inputs
                     .iter()
                     .find_map(|y| y.as_ref().and_then(|y| x.match_fem_inputs(y)))
-                    .ok_or(StateSpaceError::FemInputs(x.clone()))
+                    .map(|io| (io, x.into()))
+                    .ok_or(StateSpaceError::FemInputs(x.into()))
             })
-            .collect::<Result<Vec<Vec<IO>>>>()?
+            .collect::<Result<Vec<(Vec<IO>, Option<Vec<f64>>)>>>()?
             .iter()
-            .flat_map(|v| {
-                v.iter().filter_map(|x| match x {
-                    IO::On(io) => Some(io.indices.clone()),
-                    IO::Off(_) => None,
-                })
+            .map(|(v, mat)| {
+                (
+                    v.iter()
+                        .filter_map(|x| match x {
+                            IO::On(io) => Some(io.indices.clone()),
+                            IO::Off(_) => None,
+                        })
+                        .flatten()
+                        .collect::<Vec<u32>>(),
+                    mat,
+                )
             })
-            .flatten()
-            .collect();
-        let n = fem.inputs_to_modal_forces.len() / fem.n_modes();
-        Ok(fem
-            .inputs_to_modal_forces
-            .chunks(n)
-            .flat_map(|x| {
-                indices
-                    .iter()
-                    .map(|i| x[*i as usize - 1])
-                    .collect::<Vec<f64>>()
+            .flat_map(|(indice, mat)| {
+                {
+                    let n_i = indice.len();
+                    let i2m = na::DMatrix::from_row_slice(
+                        n_mode,
+                        n_i,
+                        &(fem
+                            .inputs_to_modal_forces
+                            .chunks(n)
+                            .flat_map(|x| {
+                                indice
+                                    .iter()
+                                    .map(|i| x[*i as usize - 1])
+                                    .collect::<Vec<f64>>()
+                            })
+                            .collect::<Vec<f64>>()),
+                    );
+                    match mat {
+                        Some(mat) => {
+                            i2m * na::DMatrix::from_column_slice(n_i, mat.len() / n_i, &mat)
+                        }
+                        None => i2m,
+                    }
+                }
+                .as_slice()
+                .to_vec()
             })
             .collect())
+        /*Ok(fem
+        .inputs_to_modal_forces
+        .chunks(n)
+        .flat_map(|x| {
+            indices
+                .iter()
+                .map(|i| x[*i as usize - 1])
+                .collect::<Vec<f64>>()
+        })
+        .collect())*/
     }
-    fn modes2io(fem: &fem::FEM, dos_outputs: &[Tags]) -> Result<Vec<Vec<f64>>> {
+    fn modes2io(fem: &fem::FEM, dos_outputs: &StateSpaceIO) -> Result<Vec<Vec<f64>>> {
         use crate::io::IO;
         let n = fem.n_modes();
         let q: Vec<_> = fem.modal_disp_to_outputs.chunks(n).collect();
@@ -261,23 +311,42 @@ impl DiscreteStateSpace {
                 fem.outputs
                     .iter()
                     .find_map(|y| y.as_ref().and_then(|y| x.match_fem_outputs(y)))
-                    .ok_or(StateSpaceError::FemOutputs(x.clone()))
+                    .map(|io| (io, x.into()))
+                    .ok_or(StateSpaceError::FemOutputs(x.into()))
             })
-            .collect::<Result<Vec<Vec<IO>>>>()?
+            .collect::<Result<Vec<(Vec<IO>, Option<Vec<f64>>)>>>()?
             .into_iter()
-            .map(|v| {
-                v.into_iter()
-                    .filter_map(|x| match x {
-                        IO::On(io) => Some(io.indices),
-                        IO::Off(_) => None,
-                    })
-                    .flatten()
-                    .collect::<Vec<u32>>()
+            .map(|(v, mat)| {
+                (
+                    v.into_iter()
+                        .filter_map(|x| match x {
+                            IO::On(io) => Some(io.indices),
+                            IO::Off(_) => None,
+                        })
+                        .flatten()
+                        .collect::<Vec<u32>>(),
+                    mat,
+                )
             })
-            .map(|i| {
-                i.iter()
-                    .flat_map(|i| q[*i as usize - 1].to_owned())
-                    .collect::<Vec<f64>>()
+            .map(|(i, mat)| {
+                (
+                    i.iter()
+                        .flat_map(|i| q[*i as usize - 1].to_owned())
+                        .collect::<Vec<f64>>(),
+                    mat,
+                )
+            })
+            .map(|(m2o, mat)| match mat {
+                Some(mat) => {
+                    let n_mode = fem.n_modes();
+                    let n_o = m2o.len() / n_mode;
+                    let mat_n_o = mat.len() / n_o;
+                    (na::DMatrix::from_column_slice(mat_n_o, n_o, &mat)
+                        * na::DMatrix::from_row_slice(n_o, n_mode, &m2o))
+                    .as_slice()
+                    .to_vec()
+                }
+                None => m2o,
             })
             .collect())
     }
@@ -295,22 +364,40 @@ impl DiscreteStateSpace {
         let mut fem = self
             .fem
             .map_or(Err(StateSpaceError::MissingArguments("FEM".to_owned())), Ok)?;
-        let dos_inputs = self.u.map_or(
-            Err(StateSpaceError::MissingArguments("inputs".to_owned())),
-            Ok,
-        )?;
-        let dos_outputs = self.y.map_or(
-            Err(StateSpaceError::MissingArguments("outputs".to_owned())),
-            Ok,
-        )?;
-        Self::select_fem_io(&mut fem, &dos_inputs, &dos_outputs);
-        let forces_2_modes = na::DMatrix::from_row_slice(
+        let dos_inputs: Vec<IO<()>> = self
+            .u
+            .as_ref()
+            .map_or(
+                Err(StateSpaceError::MissingArguments("inputs".to_owned())),
+                Ok,
+            )?
+            .iter()
+            .map(|io| {
+                let t: IO<()> = io.into();
+                t
+            })
+            .collect();
+        let dos_outputs: Vec<IO<()>> = self
+            .y
+            .as_ref()
+            .map_or(
+                Err(StateSpaceError::MissingArguments("outputs".to_owned())),
+                Ok,
+            )?
+            .iter()
+            .map(|io| {
+                let t: IO<()> = io.into();
+                t
+            })
+            .collect();
+        Self::select_fem_io(&mut fem, self.u.as_ref().unwrap(), self.y.as_ref().unwrap());
+        let forces_2_modes = na::DMatrix::from_column_slice(
             fem.n_modes(),
             fem.n_inputs(),
-            &Self::io2modes(&fem, &dos_inputs)?,
+            &Self::io2modes(&fem, self.u.as_ref().unwrap())?,
         );
         log::info!("forces 2 modes: {:?}", forces_2_modes.shape());
-        let fem_modes2io = Self::modes2io(&fem, &dos_outputs)?;
+        let fem_modes2io = Self::modes2io(&fem, self.y.as_ref().unwrap())?;
         let sizes: Vec<_> = fem_modes2io
             .iter()
             .map(|f| f.len() / fem.n_modes())
