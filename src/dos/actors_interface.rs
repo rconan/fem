7d0cfe79b43f1ce0fb2a73bb4d710c526dae450b
Rod Conan <rconan@gmto.org>
2022-09-30 04:38:27 +0000

    WITH DOS CLIENTS IO

--- src/dos/actors_interface.rs
+++ src/dos/actors_interface.rs
@@ -93,28 +93,9 @@ use crate::{
 };
 use dos_actors::{
     io::{Data, Read, UniqueIdentifier, Write},
-    Size, Update, UID,
+    Size, Update,
 };
 use std::sync::Arc;
-
-impl<S> Size<fem_io::OSSM1Lcl> for DiscreteModalSolver<S>
-where
-    DiscreteModalSolver<S>: Iterator,
-    S: Solver + Default,
-{
-    fn len(&self) -> usize {
-        42
-    }
-}
-impl<S> Size<fem_io::MCM2Lcl6D> for DiscreteModalSolver<S>
-where
-    DiscreteModalSolver<S>: Iterator,
-    S: Solver + Default,
-{
-    fn len(&self) -> usize {
-        42
-    }
-}
 
 impl<S> Update for DiscreteModalSolver<S>
 where
@@ -149,10 +130,10 @@ where
     }
 }
 
-// MOUNT CONTROL ----------------------------------------------------------------
+// # MOUNT CONTROL
+use dos_clients_io::{MountEncoders, MountTorques};
 
-#[cfg(feature = "mount")]
-impl<S> Get<mount::MountEncoders> for DiscreteModalSolver<S>
+impl<S> Get<MountEncoders> for DiscreteModalSolver<S>
 where
     S: Solver + Default,
 {
@@ -167,18 +148,16 @@ where
         Some(encoders)
     }
 }
-#[cfg(feature = "mount")]
-impl<S> Write<mount::MountEncoders> for DiscreteModalSolver<S>
+impl<S> Write<MountEncoders> for DiscreteModalSolver<S>
 where
     S: Solver + Default,
 {
-    fn write(&mut self) -> Option<Arc<Data<mount::MountEncoders>>> {
-        <DiscreteModalSolver<S> as Get<mount::MountEncoders>>::get(self)
+    fn write(&mut self) -> Option<Arc<Data<MountEncoders>>> {
+        <DiscreteModalSolver<S> as Get<MountEncoders>>::get(self)
             .map(|data| Arc::new(Data::new(data)))
     }
 }
-#[cfg(feature = "mount")]
-impl<S: Solver + Default> Set<mount::MountTorques> for DiscreteModalSolver<S> {
+impl<S: Solver + Default> Set<MountTorques> for DiscreteModalSolver<S> {
     fn set(&mut self, u: &[f64]) {
         let (azimuth, others) = u.split_at(12);
         <DiscreteModalSolver<S> as Set<fem_io::OSSAzDriveTorque>>::set(self, azimuth);
@@ -187,21 +166,19 @@ impl<S: Solver + Default> Set<mount::MountTorques> for DiscreteModalSolver<S> {
         <DiscreteModalSolver<S> as Set<fem_io::OSSRotDriveTorque>>::set(self, gir);
     }
 }
-#[cfg(feature = "mount")]
-impl<S> Read<mount::MountTorques> for DiscreteModalSolver<S>
+impl<S> Read<MountTorques> for DiscreteModalSolver<S>
 where
     S: Solver + Default,
 {
-    fn read(&mut self, data: Arc<Data<mount::MountTorques>>) {
-        <DiscreteModalSolver<S> as Set<mount::MountTorques>>::set(self, &**data);
+    fn read(&mut self, data: Arc<Data<MountTorques>>) {
+        <DiscreteModalSolver<S> as Set<MountTorques>>::set(self, &data);
     }
 }
 
-// M1 CONTROL ----------------------------------------------------------------
-
-#[derive(UID)]
-pub enum M1SegmentsAxialD {}
-impl<S> Get<M1SegmentsAxialD> for DiscreteModalSolver<S>
+// # M1 CONTROL
+use dos_clients_io::{M1ModeShapes, M1RigidBodyMotions, M2RigidBodyMotions};
+//  * M1 mode shape
+impl<S> Get<M1ModeShapes> for DiscreteModalSolver<S>
 where
     S: Solver + Default,
 {
@@ -228,22 +205,197 @@ where
         Some(encoders)
     }
 }
-impl<S> Write<M1SegmentsAxialD> for DiscreteModalSolver<S>
+impl<S> Write<M1ModeShapes> for DiscreteModalSolver<S>
 where
     S: Solver + Default,
 {
-    fn write(&mut self) -> Option<Arc<Data<M1SegmentsAxialD>>> {
-        <DiscreteModalSolver<S> as Get<M1SegmentsAxialD>>::get(self)
+    fn write(&mut self) -> Option<Arc<Data<M1ModeShapes>>> {
+        <DiscreteModalSolver<S> as Get<M1ModeShapes>>::get(self)
             .map(|data| Arc::new(Data::new(data)))
     }
 }
-#[cfg(feature = "crseo")]
-impl<S> Write<crseo::M1modes> for DiscreteModalSolver<S>
+//  * M1 rigid body motions
+impl<S> Size<M1RigidBodyMotions> for DiscreteModalSolver<S>
+where
+    DiscreteModalSolver<S>: Iterator,
+    S: Solver + Default,
+{
+    fn len(&self) -> usize {
+        42
+    }
+}
+impl<S> Write<M1RigidBodyMotions> for DiscreteModalSolver<S>
 where
     S: Solver + Default,
 {
-    fn write(&mut self) -> Option<Arc<Data<crseo::M1modes>>> {
-        <DiscreteModalSolver<S> as Get<M1SegmentsAxialD>>::get(self)
+    fn write(&mut self) -> Option<Arc<Data<M1RigidBodyMotions>>> {
+        <DiscreteModalSolver<S> as Get<fem_io::OSSM1Lcl>>::get(self)
             .map(|data| Arc::new(Data::new(data)))
+    }
+}
+//  * M2 rigid body motions
+impl<S> Size<M2RigidBodyMotions> for DiscreteModalSolver<S>
+where
+    DiscreteModalSolver<S>: Iterator,
+    S: Solver + Default,
+{
+    fn len(&self) -> usize {
+        42
+    }
+}
+impl<S> Write<M2RigidBodyMotions> for DiscreteModalSolver<S>
+where
+    S: Solver + Default,
+{
+    fn write(&mut self) -> Option<Arc<Data<M2RigidBodyMotions>>> {
+        <DiscreteModalSolver<S> as Get<fem_io::MCM2Lcl6D>>::get(self)
+            .map(|data| Arc::new(Data::new(data)))
+    }
+}
+
+// # M2 CONTROL
+use dos_clients_io::{M2PositionerForces, M2PositionerNodes};
+// ## M2 positioner
+//  * forces
+impl<S> Read<M2PositionerForces> for DiscreteModalSolver<S>
+where
+    S: Solver + Default,
+{
+    fn read(&mut self, data: Arc<Data<M2PositionerForces>>) {
+        <DiscreteModalSolver<S> as Set<fem_io::MCM2SmHexF>>::set(self, &data)
+    }
+}
+// * nodes
+impl<S> Write<M2PositionerNodes> for DiscreteModalSolver<S>
+where
+    S: Solver + Default,
+{
+    fn write(&mut self) -> Option<Arc<Data<M2PositionerNodes>>> {
+        <DiscreteModalSolver<S> as Get<fem_io::MCM2SmHexD>>::get(self)
+            .map(|data| Arc::new(Data::new(data)))
+    }
+}
+#[cfg(feature = "fsm")]
+pub mod fsm {
+    use super::*;
+    use dos_clients_io::{M2FSMPiezoForces, M2FSMPiezoNodes};
+    // ## M2 FSM PZT
+    // *  forces
+    impl<S> Read<M2FSMPiezoForces> for DiscreteModalSolver<S>
+    where
+        S: Solver + Default,
+    {
+        fn read(&mut self, data: Arc<Data<M2FSMPiezoForces>>) {
+            <DiscreteModalSolver<S> as Set<fem_io::MCM2PZTF>>::set(self, &data)
+        }
+    }
+    // * nodes
+    impl<S> Write<M2FSMPiezoNodes> for DiscreteModalSolver<S>
+    where
+        S: Solver + Default,
+    {
+        fn write(&mut self) -> Option<Arc<Data<M2FSMPiezoNodes>>> {
+            <DiscreteModalSolver<S> as Get<fem_io::MCM2PZTD>>::get(self)
+                .map(|data| Arc::new(Data::new(data)))
+        }
+    }
+}
+#[cfg(feature = "asm")]
+pub mod asm {
+    use super::*;
+    use dos_clients_io::{
+        M2ASMColdPlateForces, M2ASMFaceSheetForces, M2ASMFaceSheetNodes, M2ASMRigidBodyForces,
+        M2ASMRigidBodyNodes,
+    };
+    // ## M2 ASM
+    // * rigid body
+    //  * forces
+    impl<S> Read<M2ASMRigidBodyForces> for DiscreteModalSolver<S>
+    where
+        S: Solver + Default,
+    {
+        fn read(&mut self, data: Arc<Data<M2ASMRigidBodyForces>>) {
+            <DiscreteModalSolver<S> as Set<fem_io::MCM2RB6F>>::set(self, &data)
+        }
+    }
+    // * nodes
+    impl<S> Write<M2ASMRigidBodyNodes> for DiscreteModalSolver<S>
+    where
+        S: Solver + Default,
+    {
+        fn write(&mut self) -> Option<Arc<Data<M2ASMRigidBodyNodes>>> {
+            <DiscreteModalSolver<S> as Get<fem_io::MCM2RB6D>>::get(self)
+                .map(|data| Arc::new(Data::new(data)))
+        }
+    }
+    // * cold plate
+    //  * forces
+    impl<S> Read<M2ASMColdPlateForces> for DiscreteModalSolver<S>
+    where
+        S: Solver + Default,
+    {
+        fn read(&mut self, data: Arc<Data<M2ASMColdPlateForces>>) {
+            <DiscreteModalSolver<S> as Set<fem_io::MCM2CP6F>>::set(self, &data)
+        }
+    }
+    // * face sheet
+    //  * forces
+    impl<S> Read<M2ASMFaceSheetForces> for DiscreteModalSolver<S>
+    where
+        S: Solver + Default,
+    {
+        fn read(&mut self, data: Arc<Data<M2ASMFaceSheetForces>>) {
+            <DiscreteModalSolver<S> as Set<fem_io::MCM2Lcl6F>>::set(self, &data)
+        }
+    }
+    // * nodes
+    impl<S> Write<M2ASMFaceSheetNodes> for DiscreteModalSolver<S>
+    where
+        S: Solver + Default,
+    {
+        fn write(&mut self) -> Option<Arc<Data<M2ASMFaceSheetNodes>>> {
+            <DiscreteModalSolver<S> as Get<fem_io::MCM2Lcl6D>>::get(self)
+                .map(|data| Arc::new(Data::new(data)))
+        }
+    }
+}
+
+// # CFD
+// * mount
+use dos_clients_io::{CFDM1WindLoads, CFDMountWindLoads};
+impl<S> Read<CFDMountWindLoads> for DiscreteModalSolver<S>
+where
+    S: Solver + Default,
+{
+    fn read(&mut self, data: Arc<Data<CFDMountWindLoads>>) {
+        <DiscreteModalSolver<S> as Set<fem_io::CFD2021106F>>::set(self, &data)
+    }
+}
+// * M1
+impl<S> Read<CFDM1WindLoads> for DiscreteModalSolver<S>
+where
+    S: Solver + Default,
+{
+    fn read(&mut self, data: Arc<Data<CFDM1WindLoads>>) {
+        <DiscreteModalSolver<S> as Set<fem_io::OSSM1Lcl6F>>::set(self, &data)
+    }
+}
+// * M2
+#[cfg(feature = "asm")]
+impl<S> Read<dos_clients_io::CFDM2WindLoads> for DiscreteModalSolver<S>
+where
+    S: Solver + Default,
+{
+    fn read(&mut self, data: Arc<Data<dos_clients_io::CFDM2WindLoads>>) {
+        <DiscreteModalSolver<S> as Set<fem_io::MCM2Lcl6F>>::set(self, &data)
+    }
+}
+#[cfg(feature = "fsm")]
+impl<S> Read<dos_clients_io::CFDM2WindLoads> for DiscreteModalSolver<S>
+where
+    S: Solver + Default,
+{
+    fn read(&mut self, data: Arc<Data<dos_clients_io::CFDM2WindLoads>>) {
+        <DiscreteModalSolver<S> as Set<fem_io::MCM2LclForce6F>>::set(self, &data)
     }
 }
