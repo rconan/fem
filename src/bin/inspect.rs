d04bff61507eaefd9f168366cd2a28d1a8ce2670
Rod Conan <rconan@gmto.org>
2023-01-16 14:57:39 -0800

    ADD FREQUENCY HISTOGRAM TABLE

--- src/bin/inspect.rs
+++ src/bin/inspect.rs
@@ -3,15 +3,40 @@ use gmt_fem::{
     FEM,
 };
 
+fn frequency_base2_histogram<'a>(nu: &[f64], max_nu: f64) -> Vec<usize> {
+    (0..)
+        .map_while(|i| {
+            let upper = 2i32 << i;
+            let lower = if i == 0 { 0 } else { upper / 2 };
+            if lower as f64 > max_nu {
+                None
+            } else {
+                Some(
+                    nu.iter()
+                        .filter(|&&nu| nu >= lower as f64 && nu < upper as f64)
+                        .enumerate()
+                        .last()
+                        .map_or_else(|| 0, |(i, _)| i + 1),
+                )
+            }
+        })
+        .collect()
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let fem = FEM::from_env()?;
+
     let nu = fem.eigen_frequencies.clone();
+    let max_nu = nu.iter().cloned().fold(f64::NEG_INFINITY, f64::max);
+    let mut nu_hist = vec![frequency_base2_histogram(&nu, max_nu)];
+
     let state_space = DiscreteStateSpace::<Exponential>::from(fem);
     state_space.fem_info();
     let hsv = state_space.hankel_singular_values()?;
 
     let max_hsv = hsv.iter().cloned().fold(f64::NEG_INFINITY, f64::max);
-    let nu_hsv: Vec<_> = nu.into_iter().zip(hsv.into_iter()).collect();
+    let nu_hsv: Vec<_> = nu.iter().cloned().zip(hsv.into_iter()).collect();
+
     let n_mode = nu_hsv.len();
 
     print!(
@@ -29,6 +54,15 @@ HANKEL SINGULAR VALUES MODEL REDUCTION
             .iter()
             .filter(|(_, hsv)| *hsv > max_hsv * hsv_threshold)
             .collect();
+
+        nu_hist.push(frequency_base2_histogram(
+            red_nu_hsv
+                .iter()
+                .map(|(nu, _)| *nu)
+                .collect::<Vec<f64>>()
+                .as_slice(),
+            max_nu,
+        ));
 
         let min_nu = red_nu_hsv
             .iter()
@@ -56,6 +90,27 @@ HANKEL SINGULAR VALUES MODEL REDUCTION
             (min_nu, max_nu)
         );
     }
+
+    println!(" {}", "-".repeat(43));
+    println!(" |{:^41}|", "Models Frequency Histograms");
+    println!(" |{}|", "-".repeat(41));
+    println!(" |{:^6}|{:^34}|", "Bin", "Models");
+    print!(" |{:^5}", "Hz");
+    for i in 0..nu_hist.len() {
+        print!(" | {:^4}", i);
+    }
+    println!(" |");
+    println!(" {}|", "|------".repeat(1 + nu_hist.len()));
+    let n_bin = nu_hist[0].len();
+    for i in 0..n_bin {
+        let upper = 2 << i;
+        print!(" |{:>5} ", upper);
+        for hist in &nu_hist {
+            print!("|{:>5} ", hist[i]);
+        }
+        println!("|");
+    }
+    println!(" {}", "-".repeat(43));
 
     Ok(())
 }
